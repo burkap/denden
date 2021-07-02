bb533607cacfcc9626aeca2b8360e8a75770d1b9
Burak Payzun <brkpyzn@gmail.com>
2021-07-02 17:19:14 +0300

    Remove unnecessary headers in example game

--- src/example.cpp
+++ src/example.cpp
@@ -1,41 +1,6 @@
-#define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <bullet/BulletCollision/CollisionShapes/btBoxShape.h>
-#include <bullet/BulletCollision/CollisionShapes/btSphereShape.h>
-#include <camera.h>
-#include <collisionshape.h>
-#include <component.h>
-#include <cubemap.h>
-#include <debugdrawer.h>
-#include <gameobject.h>
-#include <globals.h>
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#include <lightobject.h>
-#include <model.h>
-#include <renderer.h>
 #include <rigidbody.h>
-#include <scene.h>
-#include <shader.h>
-#include <stddef.h> /* offsetof */
-#include <texture.h>
-#include <transform.h>
-#include <util.h>
-
-#include <cmath>
-#include <cstdio>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/mat4x4.hpp>
-#include <iostream>
-#include <memory>
-#include <vector>
-#include <configparser.h>
-#include <shadermanager.h>
 #include <app.h>
+#include <util.h>
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos){}
 
